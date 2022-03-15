32c37e3e9fc4489ad97aa9304b381213d9c765c3
Javi <javier@hopu.eu>
2022-03-15 17:13:13 +0100

    Modificaciones varias de maquetación, css y funcionalidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,56 +2,61 @@ import { NgModule } from '@angular/core';
 
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
+//TODO Las rutas no están bien configuradas, revisar e incluir la configuración para rutas anidadas y tabs.
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'registration',
-    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+    loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationPageModule)
   },
   {
     path: 'forgot-password',
-    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+    loadChildren: () => import('./forgot-password/forgot-password.module').then(m => m.ForgotPasswordPageModule)
   },
   {
     path: 'searching-smart-band',
-    loadChildren: () => import('./searching-smart-band/searching-smart-band.module').then( m => m.SearchingSmartBandPageModule)
+    loadChildren: () => import('./searching-smart-band/searching-smart-band.module').then(m => m.SearchingSmartBandPageModule)
   },
   {
     path: 'college-tabs',
-    loadChildren: () => import('./college-tabs/college-tabs.module').then( m => m.CollegeTabsPageModule)
+    loadChildren: () => import('./college-tabs/college-tabs.module').then(m => m.CollegeTabsPageModule)
   },
   {
     path: 'route',
-    loadChildren: () => import('./route/route.module').then( m => m.RoutePageModule)
+    loadChildren: () => import('./route/route.module').then(m => m.RoutePageModule)
   },
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule)
   },
   {
     path: 'news',
-    loadChildren: () => import('./news/news.module').then( m => m.NewsPageModule)
+    loadChildren: () => import('./news/news.module').then(m => m.NewsPageModule)
   },
   {
     path: 'new-details',
-    loadChildren: () => import('./new-details/new-details.module').then( m => m.NewDetailsPageModule)
+    loadChildren: () => import('./new-details/new-details.module').then(m => m.NewDetailsPageModule)
   },
   {
     path: 'success-modal',
-    loadChildren: () => import('./success-modal/success-modal.module').then( m => m.SuccessModalPageModule)
+    loadChildren: () => import('./success-modal/success-modal.module').then(m => m.SuccessModalPageModule)
   },
   {
     path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
   },
   {
     path: 'resume-missions',
-    loadChildren: () => import('./resume-missions/resume-missions.module').then( m => m.ResumeMissionsPageModule)
+    loadChildren: () => import('./resume-missions/resume-missions.module').then(m => m.ResumeMissionsPageModule)
   }
 ];
 
